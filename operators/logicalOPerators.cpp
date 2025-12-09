83ccd663a2a6250ac580cac0ecbef94655fff792
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-12-09 22:29:02 +0530

    Logical operators in CPP

---
+++ operators/logicalOPerators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter two values\n" << endl;
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+
+    cout << "The value of logical AND (num1 && num2): " << (num1 && num2) << endl;
+    cout << "The value of logical OR (num1 || num2): " << (num1 || num2) << endl;
+    cout << "The value of logical NOT (!num1): " << (!num1) << endl;
+
+    return 0;
+}
